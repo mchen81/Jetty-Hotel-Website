9ce2be3f928a0154b03fd258988f3b4d84042102
Jerry <mchen81@usfca.edu>
2019-11-08 16:30:38 -0800

    Create Handlers

---
+++ src/main/java/handlers/AttractionsHandler.java
@@ -0,0 +1,13 @@
+package handlers;
+
+import rawHttpServer.HttpHandler;
+import rawHttpServer.HttpRequest;
+
+import java.io.PrintWriter;
+
+public class AttractionsHandler implements HttpHandler {
+    @Override
+    public void processRequest(HttpRequest request, PrintWriter writer) {
+
+    }
+}
---
+++ src/main/java/handlers/ReviewsHandler.java
@@ -0,0 +1,13 @@
+package handlers;
+
+import rawHttpServer.HttpHandler;
+import rawHttpServer.HttpRequest;
+
+import java.io.PrintWriter;
+
+public class ReviewsHandler implements HttpHandler {
+    @Override
+    public void processRequest(HttpRequest request, PrintWriter writer) {
+
+    }
+}
