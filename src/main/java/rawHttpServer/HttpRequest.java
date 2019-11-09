f1ea72bfdf69c5c70cb6cb959bc34e495a27bb11
Jerry <mchen81@usfca.edu>
2019-11-08 16:31:41 -0800

    Fill in Http Request

--- src/main/java/rawHttpServer/HttpRequest.java
+++ src/main/java/rawHttpServer/HttpRequest.java
@@ -1,8 +1,90 @@
 package rawHttpServer;
 
-/** A class that represents an http request */
+import exceptions.HttpRequestParingException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * A class that represents an http request
+ */
 public class HttpRequest {
     // FILL IN CODE
     // Store parameters of the request
+    private String httpRequest;
 
+    private String urlRequest;
+
+    private Map<String, String> parameterMap;
+
+    private String action = null;
+
+    private String httpCRUD = null;
+
+    public HttpRequest(String httpRequest) {
+        this.httpRequest = httpRequest;
+        parameterMap = new HashMap<>();
+        parseHttpRequest();
+    }
+
+    private void parseHttpRequest() {
+        Pattern requestPattern = Pattern.compile("([A-Za-z]*?) (.*) (\\/HTTP\\/1.1?)");
+        Matcher requestMatcher = requestPattern.matcher(httpRequest);
+
+        if (requestMatcher.find()) {
+            httpCRUD = requestMatcher.group(1);
+            urlRequest = requestMatcher.group(2);
+            parseUrlRequest();
+        } else {
+            throw new HttpRequestParingException();
+        }
+
+    }
+
+    private void parseUrlRequest() {
+        String urlRegex = "(.*?)\\?(.*)";
+        Pattern urlPattern = Pattern.compile(urlRegex);
+        Matcher urlMatcher = urlPattern.matcher(urlRequest);
+
+        if (urlMatcher.find()) {
+            action = urlMatcher.group(1);
+            String parameters = urlMatcher.group(2);
+            parseParametersToMap(parameters);
+        } else {
+            throw new HttpRequestParingException();
+        }
+
+    }
+
+    private void parseParametersToMap(String parameters) {
+        String[] parameterArray = parameters.split("&");
+        for (String parameter : parameterArray) {
+            String[] map = parameter.split("=");
+            try {
+                parameterMap.put(map[0], map[1]);
+            } catch (IndexOutOfBoundsException e) {
+                throw new HttpRequestParingException();
+            }
+
+        }
+    }
+
+
+    public String getValue(String parameter) {
+        if (parameterMap.containsKey(parameter)) {
+            return parameterMap.get(parameter);
+        }
+        return null;
+    }
+
+    public String getAction() {
+        return null != action ? action : "";
+    }
+
+    public String getHttpCRUD() {
+        return null != httpCRUD ? httpCRUD : "";
+    }
 }
+
