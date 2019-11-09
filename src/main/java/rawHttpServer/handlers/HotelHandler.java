3f7d0d74b0394d2b7f549ca992f5e4452bb2166e
Jerry <mchen81@usfca.edu>
2019-11-08 16:35:05 -0800

    Move Handlers into rawHttpServer Folder

--- src/main/java/rawHttpServer/handlers/HotelHandler.java
+++ src/main/java/rawHttpServer/handlers/HotelHandler.java
@@ -1,11 +1,11 @@
-package handlers;
+package rawHttpServer.handlers;
 
 import rawHttpServer.HttpHandler;
 import rawHttpServer.HttpRequest;
 
 import java.io.PrintWriter;
 
-public class AttractionsHandler implements HttpHandler {
+public class HotelHandler implements HttpHandler {
     @Override
     public void processRequest(HttpRequest request, PrintWriter writer) {
 
--- src/main/java/rawHttpServer/handlers/ReviewsHandler.java
+++ src/main/java/rawHttpServer/handlers/ReviewsHandler.java
@@ -1,11 +1,11 @@
-package handlers;
+package rawHttpServer.handlers;
 
 import rawHttpServer.HttpHandler;
 import rawHttpServer.HttpRequest;
 
 import java.io.PrintWriter;
 
-public class HotelHandler implements HttpHandler {
+public class ReviewsHandler implements HttpHandler {
     @Override
     public void processRequest(HttpRequest request, PrintWriter writer) {
 
