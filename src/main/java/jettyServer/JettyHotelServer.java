74ab4ea12f1d2848f504a8991e5941c1aa581b24
Jerry <mchen81@usfca.edu>
2019-11-14 18:03:26 -0800

    Update port

--- src/main/java/jettyServer/JettyHotelServer.java
+++ src/main/java/jettyServer/JettyHotelServer.java
@@ -9,7 +9,7 @@ import org.eclipse.jetty.servlet.ServletHandler;
 public class JettyHotelServer {
     // FILL IN CODE
 
-    public static final int PORT = 8084;
+    public static final int PORT = 8090;
 
     public JettyHotelServer() throws Exception {
         Server server = new Server(PORT);
